8e2400eadf1579163f8837dcbd7f9d3a57c47e81
Victor Manuel <vmherasdurillo@hotmail.com>
2023-06-19 22:50:43 +0200

    Update create-booking.ts

--- src/routes/booking/create-booking.ts
+++ src/routes/booking/create-booking.ts
@@ -40,7 +40,8 @@ const createBooking = async (req: Request, res: Response) => {
     return res.status(404).send("Usuario no encontrado");
   }
 
-  const userBooked = await bookingRepo.findOneBy({user: user_id  })
+  const userBooked = await bookingRepo.findOneByOrFail({user: user_id  })
+
   if(userBooked){
     return res.status(503).send("El usuario ya tiene una reserva")
   }
